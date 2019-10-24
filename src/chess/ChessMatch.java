dac93b5ecd0c22c139533e46e8b24ed6e5cc059d
felipeforischi <felipecoreio77@gmail.com>
2019-10-23 23:48:10 -0300

    Movimentos da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target );
 		return (ChessPiece)capturedPiece;
 	}
@@ -49,6 +50,12 @@ public class ChessMatch {
 			throw new ChessException ("Nao ha movimentos possiveis para a peca escolhida");
 		}
 		
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peca escolhida nao pode se mover para a posicao destino");
+		}
 	}
 
 	private void placeNewPiece (char column, int row, ChessPiece piece) {

403005771855bb164c5a28b3014327461539a92e
felipeforischi <felipecoreio77@gmail.com>
2019-10-23 21:33:51 -0300

    Movimentos possiveis de uma peca

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,7 +44,11 @@ public class ChessMatch {
 	private void validateSourcePosition (Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException ("Nao ha peca nesta posicao");
-		}	
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException ("Nao ha movimentos possiveis para a peca escolhida");
+		}
+		
 	}
 
 	private void placeNewPiece (char column, int row, ChessPiece piece) {
